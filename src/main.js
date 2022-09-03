0bf49ecc937e384b9b061ecf1df3ae27b67ecc10
csm <5932241060@qq.com>
2022-09-03 22:15:39 +0800

    完成商品功能开发

--- src/main.js
+++ src/main.js
@@ -3,15 +3,35 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import "@/assets/css/global.css"
-import 'element-ui/lib/theme-chalk/index.css';
+import 'element-ui/lib/theme-chalk/index.css'
 import '@/plugins/element'
 import "@/assets/fonts/iconfont.css"
 import axios from 'axios'
 import TreeTable from 'vue-table-with-tree-grid'
+//导入文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+//文本编辑器的样式
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
+
 Vue.config.productionTip = false
 axios.defaults.baseURL = "http://127.0.0.1:8888/api/private/v1",
   Vue.prototype.$http = axios
 Vue.component('tree-table', TreeTable)
+Vue.use(VueQuillEditor)
+Vue.filter('dateFormat', function (originVal) {
+  const dt = new Date(originVal * 1000)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+  return `${y}-${m}-${d}-${hh}:${mm}:${ss}`
+})
 new Vue({
   router,
   store,
